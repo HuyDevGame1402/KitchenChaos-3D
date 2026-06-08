6850e82b5aaa32aaa12850d14c09574cde63904d
HuyDevGame1402 <huyco14022004@gmail.com>
2026-06-08 20:23:18 +0700

    Plate Complete Visual

--- Assets/Scirpts/PlateKitchenObject.cs
+++ Assets/Scirpts/PlateKitchenObject.cs
@@ -1,8 +1,14 @@
 using UnityEngine;
 using System.Collections.Generic;
+using System;
 
 public class PlateKitchenObject : KitChenObject
 {
+    public event EventHandler<OnIngredientAddedEventArgs> OnIngredientAdded;
+    public class OnIngredientAddedEventArgs : EventArgs
+    {
+        public KitChenObjectSO kitchenObjectSO;
+    }
     [SerializeField] private List<KitChenObjectSO> validKitchenObjectSOList = new List<KitChenObjectSO>();
     private List<KitChenObjectSO> kitchenObjectSOList = new List<KitChenObjectSO>();
 
@@ -16,6 +22,9 @@ public class PlateKitchenObject : KitChenObject
         else
         {
             kitchenObjectSOList.Add(kitChenObjectSO);
+            OnIngredientAdded?.Invoke(this, new OnIngredientAddedEventArgs { 
+                kitchenObjectSO = kitChenObjectSO   
+            });
             return true;
         }
     }
